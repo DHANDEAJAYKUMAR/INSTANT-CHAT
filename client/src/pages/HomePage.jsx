e60c2c49ee91bae584bd6fae038015d4681cadac
Ajay <ajay@example.com>
2025-07-20 17:19:26 +0530

    Renamed Sidebar to Sidebar123

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import Sidebar from '../components/Sidebar'
+import Sidebar from '../components/Sidebar123'
 import ChatContainer from '../components/ChatContainer'
 import RightSidebar from '../components/RightSidebar'
 import { ChatContext } from '../../context/ChatContext'
