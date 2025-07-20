7294fb439c0251f88241af68b7277c7e8dc0f178
Ajay <ajay@example.com>
2025-07-20 16:45:35 +0530

    Added new feature section

--- server/server.js
+++ server/server.js
@@ -37,5 +37,10 @@ app.use("/api/messages", messageRouter);
 
 await connectDB();
 
+if(process.env.NODE_ENV !== "production"){
 const PORT = process.env.PORT || 5000;
 httpServer.listen(PORT, () => console.log("Server is running on PORT:" + PORT));
+
+}
+
+export default Server;
